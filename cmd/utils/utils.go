3bd897c5ed5052a5da7996c611138a4c75299513
ankitchauhan123 <ankit.c7@gmail.com>
2021-11-06 15:28:48 +0530

    Create utils.go

---
+++ cmd/utils/utils.go
@@ -0,0 +1,25 @@
+package utils
+
+import "os"
+
+const (
+	// BootstrapServers : bootstrap servers list
+	BootstrapServers string = "BOOTSTRAP_SERVERS"
+	// Topic : topic
+	Topic string = "TOPIC"
+	// GroupID : consumer group
+	GroupID string = "GROUP_ID"
+	// DelayMs : between sent messages
+	DelayMs string = "DELAY_MS"
+	// Partition : partition from which to consume
+	Partition string = "PARTITION"
+)
+
+// GetEnv : returns the environment variable value
+func GetEnv(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if exists {
+		return value
+	}
+	return defaultValue
+}
